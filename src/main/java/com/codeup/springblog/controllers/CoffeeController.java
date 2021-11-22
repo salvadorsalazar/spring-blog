7b0b7ef4cbe716ad19c7afa365bd4021aabda6cf
salvador salazar <salvador.g.salazar79@gmail.com>
2021-11-21 22:39:19 -0600

    updated springblog exercises

--- src/main/java/com/codeup/springblog/controllers/CoffeeController.java
+++ src/main/java/com/codeup/springblog/controllers/CoffeeController.java
@@ -1,55 +1,55 @@
-//package com.codeup.springblog.controllers;
-//
-//import com.codeup.springblog.models.Coffee;
-//import com.codeup.springblog.repositories.CoffeeRepository;
-//import com.codeup.springblog.services.EmailService;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//
-//@Controller
-//public class CoffeeController {
-//
-//    private final CoffeeRepository coffeeRepository;
-//private final EmailService emailService;
-//
-//    public CoffeeController(CoffeeRepository coffeeRepository, EmailService emailService) {
-//        this.coffeeRepository = coffeeRepository;
-//        this.emailService = emailService;
-//    }
-//
-//
-//    @GetMapping("/coffee")
-//    public String coffeeInfo(){
-//        return "coffees/create";
-//    }
-//
-//    @GetMapping("/coffee/{roast}")
-//    public String roastSelection(@PathVariable String roast, Model model){
-//        model.addAttribute("selections",coffeeRepository.findByRoast(roast));
-//        return "coffees/coffee";
-//    }
-//
-//
-//
-//    @GetMapping("coffee/create")
-//    public String showCrreateCoffeForm(Model model){
-//
-//        model.addAttribute("coffee", new Coffee());
-//        return "/coffee.create";
-//    }
-//
-//    @PostMapping("/coffee/create")
-//    public String createCoffee(ModelAttribute Coffee coffee){
-//        coffeeRepository.save(coffee);
-//        return "redirect:/coffee";
-//    }
-//
-//    @PostMapping("/coffee")
-//    public String newsletterSignup(@RequestParam(name="email") String email, Model model){
-//        model.addAttribute("email", email);
-//        emailService.prepareAndSend(email, "You have signed up for coffee emails! Thank you!");
-//        return "coffee/coffee";
-//    }
-//
-//}
+package com.codeup.springblog.controllers;
+
+import com.codeup.springblog.models.Coffee;
+import com.codeup.springblog.repositories.CoffeeRepository;
+import com.codeup.springblog.services.EmailService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class CoffeeController {
+
+    private final CoffeeRepository coffeeRepository;
+private final EmailService emailService;
+
+    public CoffeeController(CoffeeRepository coffeeRepository, EmailService emailService) {
+        this.coffeeRepository = coffeeRepository;
+        this.emailService = emailService;
+    }
+
+
+    @GetMapping("/coffee")
+    public String coffeeInfo(){
+        return "coffees/create";
+    }
+
+    @GetMapping("/coffee/{roast}")
+    public String roastSelection(@PathVariable String roast, Model model){
+        model.addAttribute("selections",coffeeRepository.findByRoast(roast));
+        return "coffees/coffee";
+    }
+
+
+
+    @GetMapping("coffee/create")
+    public String showCrreateCoffeForm(Model model){
+
+        model.addAttribute("coffee", new Coffee());
+        return "/coffee.create";
+    }
+
+    @PostMapping("/coffee/create")
+    public String createCoffee(ModelAttribute Coffee coffee){
+        coffeeRepository.save(coffee);
+        return "redirect:/coffee";
+    }
+
+    @PostMapping("/coffee")
+    public String newsletterSignup(@RequestParam(name="email") String email, Model model){
+        model.addAttribute("email", email);
+        emailService.prepareAndSend(email, "You have signed up for coffee emails! Thank you!");
+        return "coffee/coffee";
+    }
+
+}
--- src/main/java/com/codeup/springblog/repositories/CoffeeRepository.java
+++ src/main/java/com/codeup/springblog/repositories/CoffeeRepository.java
@@ -1,13 +1,13 @@
-//package com.codeup.springblog.repositories;
-//
-//import com.codeup.springblog.models.Coffee;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.List;
-//
-//public interface CoffeeRepository extends JpaRepository<Coffee,Long> {
-//
-//
-//    List<Coffee> findByRoast(String roast) ;
-//
-//}
+package com.codeup.springblog.repositories;
+
+import com.codeup.springblog.models.Coffee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CoffeeRepository extends JpaRepository<Coffee,Long> {
+
+
+    List<Coffee> findByRoast(String roast) ;
+
+}
